5ab49ac61f90b5d61150f00a2079f80393552eba
Sigureya <sigureya1208@gmail.com>
2025-02-05 21:47:09 +0900

    初回のビルド

---
+++ dist/mock/map/event/eventCommand/audio.d.ts
@@ -0,0 +1,6 @@
+import * as RpgTypes from "@sigureya/rpgtypes";
+export declare const MockPlayBGM: RpgTypes.Command_PlayBGM;
+export declare const MockPlayBGS: RpgTypes.Command_PlayBGS;
+export declare const MockPlayME: RpgTypes.Command_PlayME;
+export declare const MockPlaySE: RpgTypes.Command_PlaySE;
+export declare const MockStopBGM: RpgTypes.Command_FadeOutBGM;
---
+++ dist/rpgtypes.es.js
@@ -0,0 +1,64 @@
+const e = {
+  code: 241,
+  indent: 0,
+  parameters: [
+    {
+      name: "playBGM",
+      volume: 100,
+      pan: 0,
+      pitch: 100
+    }
+  ]
+}, a = {
+  code: 245,
+  indent: 0,
+  parameters: [
+    {
+      name: "playBGS",
+      volume: 100,
+      pan: 0,
+      pitch: 100
+    }
+  ]
+}, o = {
+  code: 249,
+  indent: 0,
+  parameters: [
+    {
+      name: "playME",
+      volume: 100,
+      pan: 0,
+      pitch: 100
+    }
+  ]
+}, n = {
+  code: 250,
+  indent: 0,
+  parameters: [
+    {
+      name: "playSE",
+      volume: 100,
+      pan: 0,
+      pitch: 100
+    }
+  ]
+}, t = {
+  code: 242,
+  indent: 0,
+  parameters: [0]
+}, c = {
+  name: "test",
+  volume: 100,
+  pan: 0,
+  pitch: 100
+}, p = [255, 255, 255, 255];
+export {
+  c as MockAudioParams,
+  p as MockColorRGBA,
+  e as MockPlayBGM,
+  a as MockPlayBGS,
+  o as MockPlayME,
+  n as MockPlaySE,
+  t as MockStopBGM
+};
+//# sourceMappingURL=rpgtypes.es.js.map
