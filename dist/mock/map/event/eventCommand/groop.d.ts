a932c6dd7617c98004e971c1fa20e8b42beb6c7c
Sigureya <sigureya1208@gmail.com>
2025-02-17 17:56:18 +0900

    モックを修正。変数名が間違っていた

--- dist/mock/map/event/eventCommand/groop.d.ts
+++ dist/mock/map/event/eventCommand/groop.d.ts
@@ -1,5 +1,5 @@
 import * as RpgTypes from "@sigureya/rpgtypes";
 export declare const MockShowMessage: RpgTypes.Command_ShowMessage;
 export declare const MockShowMessageBody: RpgTypes.Command_ShowMessageBody;
-export declare const MpockComment: RpgTypes.Command_Comment;
+export declare const MockComment: RpgTypes.Command_Comment;
 export declare const MockCommentBody: RpgTypes.Command_CommentBody;
